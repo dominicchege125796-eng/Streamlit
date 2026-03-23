08e741aabfc3437e90637c27153d41dce006944e
Dominic Chege <dominicchege125796@gmail.com>
2026-03-23 08:18:35 +0300

    Update upgrade.py

--- upgrade.py
+++ upgrade.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Pro Student Analytics", layout="wide")
 
-st.title("📊 PRO Student Performance Dashboard")
+st.title("Student Performance Dashboard")
 
 # ---------------- GRADE FUNCTION ----------------
 def grade(avg):
